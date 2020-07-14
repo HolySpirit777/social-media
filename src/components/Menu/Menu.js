7090b3f0fa0559062c7bf61a5da1af440a74be65
Wilson Toribio <43834329+HolySpirit777@users.noreply.github.com>
2020-07-14 19:17:43 -0400

    Start 1

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  import './Menu.css';
+
+const Menu = () => {
+    return (
+        <div>
+            <ul className="menu-nav">
+                <li className="menu-nav-element"><a className="menu-nav-link" href="#">Posts</a> </li>
+                <li className="menu-nav-element"><a className="menu-nav-link" href="#">Profile</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
